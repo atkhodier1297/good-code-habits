c62f943e14dd4548d7709c88a3efc738094a361d
Adam Khodier <khodieradam1297@gmail.com>
2023-08-29 23:39:33 -0400

    trying with more names

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const nemo = ["nemo"];
+const everyone = ["bloat", "dory", "bruce", 
+"marlin", "gill", "nemo"];
 
 function findNemo(array) {
   let t0 = performance.now();
@@ -8,8 +10,9 @@ function findNemo(array) {
     }
   }
   let t1 = performance.now();
-  console.log("Call to find Nemo took" + (t1 - t0) + "milliseconds");
+  console.log("Call to find Nemo took " + (t1 - t0) + "milliseconds");
 }
 findNemo(nemo);
+findNemo(everyone)
 
 //practicing good coding concepts just started ZTM DS and ALG course
